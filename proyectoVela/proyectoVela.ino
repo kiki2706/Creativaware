4bd94b545c66a2a0c9f7386e671ddc8cb6e3953b
noew77 <noepaniiagua@gmail.com>
2023-10-07 19:12:54 +0200

    velamparaV1.1:Software acabado, preguntar la funcion llama constante(o comentarla)

--- proyectoVela/proyectoVela.ino
+++ proyectoVela/proyectoVela.ino
@@ -4,26 +4,56 @@
 //A2 Lectura de la NTC
 //D2 Encendido de la lampara
 
+//Cuando calor-->se enciende
+//Si soplas--> se apaga
+//Si soplas y aun hay calor se apaga 
+
+#define EntradaMicro A1
+#define EntradaNTC A2
+#define rele 2
+
+int medida[2] = {0, 0};
+unsigned long delayInicial = 0;
+float tensionMicrofono, tensionNTC;
+
+
 void setup() {
   Serial.begin(9600);
+  delayInicial = millis();
+  
 }
 
 void loop() {
-  float tensionMicrofono, tensionNTC;
-  tensionMicrofono = analogRead(A1);
-  tensionNTC = analogRead(A2);
-  Serial.println(tensionNTC);
+  tensionMicrofono = analogRead(EntradaMicro);
+  tensionNTC = analogRead(EntradaNTC);
+  //Serial.println(tensionNTC);
   //La lectura del microfono es un valor de [0-2¹0] y con un fondo de escala de 5v
   //Nuesto valor base (660~) corresponde a 850mV y (770~) a 4V
-
   //La lectura de la NTC va de 800~ (4v) a 500 (2.3v)
 
   
-  if(tensionNTC < 550){
-    digitalWrite(2, HIGH);
-  }
+  if((millis() - delayInicial) >= 250){//Cada 200ms tomamos una medida
+    delayInicial = millis();
+    
+    if((medida[1] == 0) && (medida[0] != 0)){//Si tenemos una sola medida...
+       medida[1] = tensionNTC;
 
-  if(tensionMicrofono > 720){
-    digitalWrite(2, LOW);
-  }
-}
+        if((medida[0]-medida[1]) > 15)//Si la pendiente decrece no es por un pulso espurio...
+            digitalWrite(rele, HIGH);
+          else if((medida[1] < 300) && ((medida[0]-medida[1]) < 1))//Si no hay pendiente pero se esta aplicando fuego...
+           digitalWrite(rele, HIGH);
+
+         medida[0] = 0;
+         medida[1] = 0;
+    }
+    else//No tenemos ninguna medida
+    medida[0] = tensionNTC;
+ }
+      
+  
+  
+  if(tensionMicrofono > 720){//Siempre que se sople se apagará
+    digitalWrite(rele, LOW);
+  } 
+  
+}//void loop
