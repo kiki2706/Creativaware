e8412527bc84307f798a10e866706aad3af9e9b1
noew77 <noepaniiagua@gmail.com>
2023-10-15 20:30:40 +0200

    velamparaV1.2:Proyecto terminado a falta de algun ajuste

--- proyectoVela/proyectoVela.ino
+++ proyectoVela/proyectoVela.ino
@@ -8,9 +8,9 @@
 //Si soplas--> se apaga
 //Si soplas y aun hay calor se apaga 
 
-#define EntradaMicro A1
-#define EntradaNTC A2
-#define rele 2
+#define EntradaMicro A2
+#define EntradaNTC A1
+#define rele 11
 
 int medida[2] = {0, 0};
 unsigned long delayInicial = 0;
@@ -20,27 +20,28 @@ float tensionMicrofono, tensionNTC;
 void setup() {
   Serial.begin(9600);
   delayInicial = millis();
+  pinMode(11, OUTPUT);
   
 }
 
 void loop() {
   tensionMicrofono = analogRead(EntradaMicro);
   tensionNTC = analogRead(EntradaNTC);
-  //Serial.println(tensionNTC);
+  Serial.println(tensionNTC);
   //La lectura del microfono es un valor de [0-2¹0] y con un fondo de escala de 5v
   //Nuesto valor base (660~) corresponde a 850mV y (770~) a 4V
   //La lectura de la NTC va de 800~ (4v) a 500 (2.3v)
 
   
-  if((millis() - delayInicial) >= 250){//Cada 200ms tomamos una medida
+  if((millis() - delayInicial) >= 150){//Cada 200ms tomamos una medida
     delayInicial = millis();
     
     if((medida[1] == 0) && (medida[0] != 0)){//Si tenemos una sola medida...
        medida[1] = tensionNTC;
 
-        if((medida[0]-medida[1]) > 15)//Si la pendiente decrece no es por un pulso espurio...
+        if((medida[0]-medida[1]) > 5)//Si la pendiente decrece no es por un pulso espurio...
             digitalWrite(rele, HIGH);
-          else if((medida[1] < 300) && ((medida[0]-medida[1]) < 1))//Si no hay pendiente pero se esta aplicando fuego...
+          else if((medida[1] < 150) && ((medida[0]-medida[1]) > 1))//Si no hay pendiente pero se esta aplicando fuego...
            digitalWrite(rele, HIGH);
 
          medida[0] = 0;
